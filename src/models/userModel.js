ad8abe3c364f79d715916ff530f7a5501d5194c7
H-Vasev <hristovasev999@gmail.com>
2025-09-18 15:39:16 -0400

    fixing typos

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongosse");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     userName: {
@@ -28,4 +28,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-modeule.exports = User;
+module.exports = User;
